a3fe59eb0c053eff1f1100c4ce2ae5c630a2545b
SWTD 18E <pseude18@gmail.com>
2023-03-01 03:12:43 -0700

    sec

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,14 @@
 'use strict';
 
+
+async function getStock(stock) {
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+  );
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice };
+}
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
