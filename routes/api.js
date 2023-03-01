ea71c62f80695b694541c4ff2aa760eed80fb422
SWTD 18E <pseude18@gmail.com>
2023-03-01 08:59:55 -0700

    kjdx

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,27 @@
 'use strict';
 
-async function getStock(stock) {
-  const response = await fetch(
-    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
-  );
-  const { symbol, latestPrice } = await response.json();
-  return { symbol, latestPrice };
+async function saveStock() {
+  
 }
 
 module.exports = function (app) {
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+  app.route("/api/stock-prices").get(async function (req, res) {
+    const { stock, like } = req.query;
+    const response = await fetch(
+      `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+    );
+    const { symbol, latestPrice } = await response.json();
+    if(!symbol) {
+      res.json({stockData: {likes: like ? true:false}});
+      return;
+    }
+    res.json({
+      stockData: {
+        stock:symbol,
+        price:latestPrice,
+        likes:12,
+      }
     });
-    
+  });
 };
--- test.js
+++ test.js
@@ -1,10 +1,34 @@
-async function getStock(stock) {
-    const response = await fetch(
-      `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
-    );
-    const { symbol, latestPrice } = await response.json();
-    return { symbol, latestPrice };
+const express = require('express');
+const app = express();
+
+app.get('/',async function(req,res) {
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+  );
+  const { symbol, latestPrice } = await response.json();
+  if(!symbol) {
+    console.log(like);
+    res.json({stockData: {likes:like ? 1:0}});
+    return;
   }
+  res.json({
+    stockData:{
+      stock:"",
+      price:1,
+      likes:1,
+    }
+  });
+  console.log(res.json());
+});
  
-getStock("goog");
+var like=true;
+var res={
+  stockData:{
+    stock:"21",
+    price:1,
+    likes:like ? 1:0,
+  }
+};
+console.log(res);
+//getStock("goog");
 
